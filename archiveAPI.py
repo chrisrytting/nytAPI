3a61c1739c0fb62175ebc90e1a3c2c433f61f022
Chris Rytting <christophermichaelrytting@gmail.com>
2019-03-27 23:15:14 -0600

    first working version of archiveAPI

--- archiveAPI.py
+++ archiveAPI.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
+import sys
+import json
 import requests
 
-
-# In[7]:
-
-
-# API_KEY = 'v2wsp3yB5OWqhGf99uzEpFaGwddFnyCh'
 API_SIGNUP_URL = 'https://developer.nytimes.com/get-started'
 
 class ArchiveAPI():
@@ -28,53 +19,29 @@ class ArchiveAPI():
         returns a request object
         """
         
-        resp = requests.get('https://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}'.format(self.key,year,month))
-        if resp.status_code != 200:
-            print("Error:")
-            if resp.status_code == 401:
-                raise Exception('Unauthorized request, check                api-key is set.')
-            if resp.status_code == 429:
-                raise Exception('Too many requests. You reached                 your per minute or per day rate limit.')
-        
-                            
-        
-        
-
-
-# In[27]:
-
+        resp = requests.get('https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key={}'.format(self.key))
+        print('inside fetch_articles')
+        try:
+            if resp.status_code != 200:
+                print("Error:")
+                if resp.status_code == 401:
+                    raise Exception('Unauthorized request, check                    api-key is set.')
+                if resp.status_code == 429:
+                    raise Exception('Too many requests. You reached                     your per minute or per day rate limit.')
+            else:
+                return resp
+        except:
+            print('No Resp')
 
 if __name__ == '__main__':
-    for key, value in resp.headers.items():
-        print ('Key: {} \t Value: {}'.format(key, value))
+    print(sys.argv)
+    if len(sys.argv) != 4:
+        raise IOError('User must specify API key, month, and year')
+    API_KEY,MONTH,YEAR = sys.argv[1:]
+    #TODO check that inputs are valid.
+    archiveAPI = ArchiveAPI(API_KEY)
+    articles = archiveAPI.fetch_articles(MONTH, YEAR)
 
-
-    # In[32]:
-
-
-    type(resp.json())
-
-
-    # In[40]:
-
-
-    resp.json()['response'].keys()
-
-
-    # In[45]:
-
-
-    len(resp.json()['response']['docs'])
-
-
-    # In[49]:
-
-
-    resp.json()['response']['docs'][0]['web_url']
-
-
-    # In[ ]:
-
-
-
-
+    #TODO allow user to specify output dir
+    with open('response.json', 'w') as outfile:
+        json.dump(articles.json()['response'], outfile)
